d92e15520e02ceb33bc06ebce6ade08dd14d0575
sonett-i <sam@sonetti.net>
2024-04-30 14:43:12 +0930

    add login form

--- ChatTCP_Client/ChatTCP_Client/LoginForm/Login.xaml.cs
+++ ChatTCP_Client/ChatTCP_Client/LoginForm/Login.xaml.cs
@@ -10,19 +10,28 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ChatTCP_Client
+namespace ChatTCP_Client.LoginForm
 {
 	/// <summary>
-	/// Interaction logic for MainWindow.xaml
+	/// Interaction logic for Login.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class Login : Window
 	{
-		public MainWindow()
+		public Login()
 		{
 			InitializeComponent();
+		}
+
+		private void loginButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void registerButton_Click(object sender, RoutedEventArgs e)
+		{
+
 		}
 	}
 }
