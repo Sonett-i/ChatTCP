28dc66efa0ea2a1514dc27c88ecb3f76ee9f2962
sonett-i <sam@sonetti.net>
2024-05-13 18:10:27 +0930

    Start coding tictactoo

---
+++ ChatTCP_Server/Data/Commands/Commands.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatTCP_Server.Data
+{
+	public static class Commands
+	{
+		public enum COMMAND
+		{
+			COMMAND_HELP,
+			COMMAND_WHISPER,
+			COMMAND_GAME,
+			COMMAND_GAME_ACCEPT,
+			COMMAND_KICK,
+		}
+
+
+		// Dictionary for enum to method
+
+		//void methods for commands
+
+		// Start on tictactoe
+	}
+}
---
+++ ChatTCP_Server/Data/Game/TicTacToe.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCPClientSocket;
+
+namespace ChatTCP_Server.Data.Game
+{
+	internal class TicTacToe
+	{
+		public enum Check
+		{
+			Cross,
+			Nought,
+		}
+
+		ClientSocket playerA;
+		ClientSocket playerB;
+
+		int[,] board = new int[3, 3];
+
+		public TicTacToe(ClientSocket playerA, ClientSocket playerB, int[,] board)
+		{
+			this.playerA = playerA;
+			this.playerB = playerB;
+			this.board = board;
+		}
+
+		public void Move(int x, int y)
+		{
+
+		}
+
+		// Pathfinding algo?
+		void Validate()
+		{
+			for (int x = 0; x < board.GetLength(0); x++)
+			{
+				for (int y = 0; y < board.GetLength(1); y++)
+				{
+
+				}
+			}
+		}
+	}
+}
